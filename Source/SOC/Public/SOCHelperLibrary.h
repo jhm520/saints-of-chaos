db52907e179d6a457d53ff4e407767908dfcaca9
DESKTOP-2O7DNV7\jhm52 <jhm520@gmail.com>
2022-09-25 17:22:01 -0700

    Added SOCHelperlibrary

---
+++ Source/SOC/Public/SOCHelperLibrary.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "SOCHelperLibrary.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SOC_API USOCHelperLibrary : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+	
+};
