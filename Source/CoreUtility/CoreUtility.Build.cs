594853d7f643466b497cf9372af705368f65f3ae
DESKTOP-2O7DNV7\jhm52 <jhm520@gmail.com>
2023-12-28 20:40:07 -0800

    Added Core Utility Module

---
+++ Source/CoreUtility/CoreUtility.Build.cs
@@ -0,0 +1,45 @@
+using UnrealBuildTool;
+
+public class CoreUtility : ModuleRules
+{
+	public CoreUtility(ReadOnlyTargetRules Target) : base(Target)
+	{
+
+        PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+		/*
+		* Engine Modules
+		*/
+		PublicDependencyModuleNames.AddRange(new string[]
+		{
+			"Core",
+			"NetCore",
+            "CoreUObject",
+            "Engine",
+            "Json",
+            "JsonUtilities",
+            "HTTP",
+            "Slate",
+            "SlateCore",
+            "PlatformCrypto",
+            "DeveloperSettings",
+            "UMG",
+            "AudioExtensions",
+            
+            // This module should have no other dependencies other than the engine itself!
+        });
+				
+		
+		/*
+		* Gameplay Ability System
+		*/
+		PublicDependencyModuleNames.AddRange(new string[]
+		{
+			"GameplayAbilities",
+			"GameplayTags", 
+			"GameplayTasks"
+		});
+		
+		PublicIncludePaths.Add("CoreUtility");
+	}
+}
---
+++ Source/CoreUtility/CoreUtilityModule.cpp
@@ -0,0 +1,15 @@
+#include "CoreUtilsModule.h"
+
+#define LOCTEXT_NAMESPACE "CoreUtilityModule"
+
+void FCoreUtilityModule::StartupModule()
+{
+}
+
+void FCoreUtilityModule::ShutdownModule()
+{
+}
+
+#undef LOCTEXT_NAMESPACE
+	
+IMPLEMENT_MODULE(FCoreUtilityModule, CoreUtility)
