2038b93e6cbd478abe67735c570808ec4069c3c6
DESKTOP-2O7DNV7\jhm52 <jhm520@gmail.com>
2024-04-12 17:06:44 -0700

    Added selection system plugin

---
+++ Plugins/SelectionSystemPlugin/Source/SelectionSystemPlugin/Private/SelectionSystemPluginBPLibrary.cpp
@@ -0,0 +1,16 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "SelectionSystemPluginBPLibrary.h"
+#include "SelectionSystemPlugin.h"
+
+USelectionSystemPluginBPLibrary::USelectionSystemPluginBPLibrary(const FObjectInitializer& ObjectInitializer)
+: Super(ObjectInitializer)
+{
+
+}
+
+float USelectionSystemPluginBPLibrary::SelectionSystemPluginSampleFunction(float Param)
+{
+	return -1;
+}
+
